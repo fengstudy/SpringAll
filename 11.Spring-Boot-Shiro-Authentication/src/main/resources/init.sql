48b9aa35d1fa035d02a71a75365b1762efbee988
fengstudy <17804819931@163.com>
2022-08-17 22:43:18 +0800

    springboot-JSON USER表sql

--- 11.Spring-Boot-Shiro-Authentication/src/main/resources/init.sql
+++ 11.Spring-Boot-Shiro-Authentication/src/main/resources/init.sql
@@ -1,23 +1,18 @@
 -- ----------------------------
 -- Table structure for T_USER
 -- ----------------------------
+DROP TABLE T_USER;
 CREATE TABLE T_USER (
-   ID NUMBER NOT NULL ,
-   USERNAME VARCHAR2(20 BYTE) NOT NULL ,
-   PASSWD VARCHAR2(128 BYTE) NOT NULL ,
-   CREATE_TIME DATE NULL ,
-   STATUS CHAR(1 BYTE) NOT NULL 
+   ID INT PRIMARY KEY AUTO_INCREMENT,
+   USERNAME VARCHAR(20) NOT NULL COMMENT '用户名',
+   PASSWD VARCHAR(128) NOT NULL COMMENT '密码' ,
+   CREATE_TIME DATETIME NULL COMMENT '创建时间',
+   STATUS CHAR(1) NOT NULL COMMENT '是否有效 1：有效  0：锁定'
 );
-COMMENT ON COLUMN T_USER.USERNAME IS '用户名';
-COMMENT ON COLUMN T_USER.PASSWD IS '密码';
-COMMENT ON COLUMN T_USER.CREATE_TIME IS '创建时间';
-COMMENT ON COLUMN T_USER.STATUS IS '是否有效 1：有效  0：锁定';
+
 -- ----------------------------
 -- Records of T_USER
 -- ----------------------------
-INSERT INTO T_USER VALUES ('2', 'test', '7a38c13ec5e9310aed731de58bbc4214', TO_DATE('2017-11-19 17:20:21', 'YYYY-MM-DD HH24:MI:SS'), '0');
-INSERT INTO T_USER VALUES ('1', 'mrbird', '42ee25d1e43e9f57119a00d0a39e5250', TO_DATE('2017-11-19 10:52:48', 'YYYY-MM-DD HH24:MI:SS'), '1');
--- ----------------------------
--- Primary Key structure for table T_USER
--- ----------------------------
-ALTER TABLE T_USER ADD PRIMARY KEY (ID);
+INSERT INTO T_USER VALUES ('2', 'test', '7a38c13ec5e9310aed731de58bbc4214', '2017-11-19', '0');
+INSERT INTO T_USER VALUES ('1', 'mrbird', '42ee25d1e43e9f57119a00d0a39e5250', '2017-11-19 10:52:48', '1');
+-- --------------------------
